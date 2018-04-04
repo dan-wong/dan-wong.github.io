89f5f769dd416892fa4a52e31b1c410c97d028cb
Daniel <dan_wong@outlook.com>
2018-04-04 20:23:45 +1200

    Create EngRevue Trailer Page

---
+++ revuetrailer/js/customJS.js
@@ -0,0 +1,17 @@
+function move() {
+    var elem = document.getElementById("bar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+function noDialog() {
+    alert('No');
+}
