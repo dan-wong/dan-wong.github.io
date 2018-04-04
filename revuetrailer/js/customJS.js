74a81e054991040c9a42a2dfd97cf66ec6a83674
Daniel <dan_wong@outlook.com>
2018-04-04 20:30:15 +1200

    Added mobile warning

--- revuetrailer/js/customJS.js
+++ revuetrailer/js/customJS.js
@@ -1,3 +1,9 @@
+$( document ).ready(function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('containerWindow').innerHTML = '<h2 style="color: white; margin-top: 5rem;">Daniel was lazy and didn\'t want to make it mobile compatible</h2>';
+    }
+});
+
 function move() {
     var elem = document.getElementById("bar"); 
     var width = 1;
