2e878a028e882e7d8f2d9f493d8ae9b2801a2d2f
Daniel <dan_wong@outlook.com>
2018-04-04 20:38:05 +1200

    Add mobile workaround:

--- revuetrailer/js/customJS.js
+++ revuetrailer/js/customJS.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    if (getParameterByName('mobile')) {
+    
+    } else if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('containerWindow').innerHTML = '<h2 style="color: white; margin-top: 5rem;">Daniel was lazy and didn\'t want to make it mobile compatible</h2>';
     }
 });
@@ -21,4 +23,14 @@ function move() {
 function noDialog() {
     alert('No');
     document.getElementById("radio1").checked = true; 
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
