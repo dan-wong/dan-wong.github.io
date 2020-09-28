1b37dd529ba89d2ed73a3bbcb35a98d82853ee33
Daniel <dan_wong@outlook.com>
2020-09-28 14:58:28 +1300

    photos

---
+++ src/photos.js
@@ -0,0 +1,198 @@
+export const photos = [
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1OrCXomnPeKclu5gZt1JJ4c7BrIDKKWSA",
+    width: 6,
+    height: 4,
+    alt: "IMG_001"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=17S65JGhd3JVe5u7HJtMiT5D8yeehmu1-",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_002"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1JK15juMnkzkhif0G43uRkxa3TZTZF5R5",
+    width: 6,
+    height: 4,
+    alt: "IMG_007"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1lFgCgyWdi8bv5ByvVF5Sh79NQc_gmFp2",
+    width: 2,
+    height: 3,
+    alt: "IMG_020"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1NkB73BqQ-ZwZTu3jd4oKTDLshfJ8bWbt",
+    width: 2,
+    height: 3,
+    alt: "IMG_009"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1Z3wXR45vDBGFbsFNiCFa3EBpE2OibyKS",
+    width: 2,
+    height: 3,
+    alt: "IMG_010"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1j_Rc3lcWcD1lkU8w4rqt2JmZjEFZ7TFa",
+    width: 2,
+    height: 3,
+    alt: "IMG_021"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1TWqvyudKq1fw_OKvHSn7qbmGZ_aX2GBn",
+    width: 2,
+    height: 3,
+    alt: "IMG_011"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=10tSEaS-80jNqZk83omxGxLyg6jbvCmKa",
+  //   width: 2,
+  //   height: 3,
+  //   alt: "IMG_012"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1U2rGjIKw28s7rra1TMiV8nfr2CZd8imD",
+    width: 2,
+    height: 3,
+    alt: "IMG_013"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1mho3XTXzHXrztAx1QObi2OxBAuY8JjWX",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_004"
+  // },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1i0rexGg629WP8867-GKzHjcZdImlNxdG",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_005"
+  // },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1H_gg8GzoHUiwDsE96aLkwsqXXY580tRJ",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_008"
+  // },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=19newnWHMeOjQMGFrqTnAmhU6LXEl__q1",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_006"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1KUh9v2BNXGQYOkuD8d4V-DQ0Sl1q_vx-",
+    width: 6,
+    height: 4,
+    alt: "IMG_014"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1jdv10wVnZsBjeUnkX7aCHsoy-uiT_3Gz",
+    width: 2,
+    height: 3,
+    alt: "IMG_015"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1Tk__S2WSgnWFoazsu8dRDWMIlkY8GtbH",
+  //   width: 399,
+  //   height: 299,
+  //   alt: "IMG_016"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1DceZLEIha12_n76C6JZEB7DtbxYN49ZV",
+    width: 530,
+    height: 397,
+    alt: "IMG_017"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1FCJ2z4tXvUOT2AlhqORsxU795QD2Rw8b",
+    width: 2281,
+    height: 1520,
+    alt: "IMG_018"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1B2Jf_kh80NLl8Rp2KoDLxMhBiWSbSucP",
+    width: 3,
+    height: 2,
+    alt: "IMG_019"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1js7K8SsO6WTVGwTRjE8GNAZVHesxnHmF",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_003"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1CYrD-aF6NivQbaiZQ-Xa8IzjibN6iwtf",
+    width: 2,
+    height: 3,
+    alt: "IMG_022"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1w5zyeHnJo0k69HbvGGvbSSdDRdMzrU3r",
+    width: 2,
+    height: 3,
+    alt: "IMG_023"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=18L2xgKpZz4MPboyGBaOD5pF3sn01M8OM",
+    width: 2,
+    height: 3,
+    alt: "IMG_024"
+  },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1VBuQhRm2ze3wl9tzLkBtxZ3SvVBQMcXb",
+    width: 2,
+    height: 3,
+    alt: "IMG_025"
+  },
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=14KjU1EoG1JhLhvK7qZ55tdGfrGVPWf5X",
+  //   width: 2,
+  //   height: 3,
+  //   alt: "IMG_026"
+  // },
+  {
+    src:
+      "https://drive.google.com/uc?export=view&id=1XkCYJ2Q6ok_NgG8hzpB6ngUrmVN0MYNe",
+    width: 6,
+    height: 4,
+    alt: "IMG_027"
+  }
+  // {
+  //   src:
+  //     "https://drive.google.com/uc?export=view&id=1a5DZU7xAKK-f4LusQAoHVC7PdYGSihVH",
+  //   width: 6,
+  //   height: 4,
+  //   alt: "IMG_028"
+  // }
+];
