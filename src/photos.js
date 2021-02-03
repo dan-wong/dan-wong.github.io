c41183bcf3ec7bd3a1101ac8e93f5f28c6f72237
Daniel <dan_wong@outlook.com>
2021-02-03 22:57:33 +1300

    Update with new photos

--- src/photos.js
+++ src/photos.js
@@ -1,198 +1,157 @@
 export const photos = [
   {
-    src:
-      "https://drive.google.com/uc?export=view&id=1OrCXomnPeKclu5gZt1JJ4c7BrIDKKWSA",
+    src: "https://drscdn.500px.org/photo/1027984788/q%3D80_m%3D1500/v2?sig=a077f3b0e8bbe78c1dea85f9607d366ba1aaebfb87f5c11ceb9339b30e7db726",
     width: 6,
-    height: 4,
-    alt: "IMG_001"
+    height: 4
   },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=17S65JGhd3JVe5u7HJtMiT5D8yeehmu1-",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_002"
-  // },
   {
-    src:
-      "https://drive.google.com/uc?export=view&id=1JK15juMnkzkhif0G43uRkxa3TZTZF5R5",
+    src: "https://drscdn.500px.org/photo/1027984683/q%3D80_m%3D1500/v2?sig=d5fc29e75388071494857d4eed89109f569cacd9bf7d72f8ca95000e4fb4fe8e",
     width: 6,
-    height: 4,
-    alt: "IMG_007"
+    height: 4
   },
   {
-    src:
-      "https://drive.google.com/uc?export=view&id=1lFgCgyWdi8bv5ByvVF5Sh79NQc_gmFp2",
-    width: 2,
-    height: 3,
-    alt: "IMG_020"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1NkB73BqQ-ZwZTu3jd4oKTDLshfJ8bWbt",
-    width: 2,
-    height: 3,
-    alt: "IMG_009"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1Z3wXR45vDBGFbsFNiCFa3EBpE2OibyKS",
-    width: 2,
-    height: 3,
-    alt: "IMG_010"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1j_Rc3lcWcD1lkU8w4rqt2JmZjEFZ7TFa",
-    width: 2,
-    height: 3,
-    alt: "IMG_021"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1TWqvyudKq1fw_OKvHSn7qbmGZ_aX2GBn",
-    width: 2,
-    height: 3,
-    alt: "IMG_011"
-  },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=10tSEaS-80jNqZk83omxGxLyg6jbvCmKa",
-  //   width: 2,
-  //   height: 3,
-  //   alt: "IMG_012"
-  // },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1U2rGjIKw28s7rra1TMiV8nfr2CZd8imD",
-    width: 2,
-    height: 3,
-    alt: "IMG_013"
-  },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1mho3XTXzHXrztAx1QObi2OxBAuY8JjWX",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_004"
-  // },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1i0rexGg629WP8867-GKzHjcZdImlNxdG",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_005"
-  // },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1H_gg8GzoHUiwDsE96aLkwsqXXY580tRJ",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_008"
-  // },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=19newnWHMeOjQMGFrqTnAmhU6LXEl__q1",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_006"
-  // },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1KUh9v2BNXGQYOkuD8d4V-DQ0Sl1q_vx-",
+    src: "https://drscdn.500px.org/photo/1027985169/q%3D80_m%3D1500/v2?sig=b3b4c0ca315c6b9665b2199d2674ea448db2c235790ef1dc9da676941d8cae40",
     width: 6,
-    height: 4,
-    alt: "IMG_014"
+    height: 4
   },
   {
-    src:
-      "https://drive.google.com/uc?export=view&id=1jdv10wVnZsBjeUnkX7aCHsoy-uiT_3Gz",
+    src: "https://drscdn.500px.org/photo/1027984609/q%3D80_m%3D1000/v2?sig=954967688367ded7bb2a6ab5e7e7ecdac8dd91388562f1d76507a6ec03f51ec8",
     width: 2,
-    height: 3,
-    alt: "IMG_015"
-  },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1Tk__S2WSgnWFoazsu8dRDWMIlkY8GtbH",
-  //   width: 399,
-  //   height: 299,
-  //   alt: "IMG_016"
-  // },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1DceZLEIha12_n76C6JZEB7DtbxYN49ZV",
-    width: 530,
-    height: 397,
-    alt: "IMG_017"
+    height: 3
   },
   {
-    src:
-      "https://drive.google.com/uc?export=view&id=1FCJ2z4tXvUOT2AlhqORsxU795QD2Rw8b",
-    width: 2281,
-    height: 1520,
-    alt: "IMG_018"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1B2Jf_kh80NLl8Rp2KoDLxMhBiWSbSucP",
-    width: 3,
-    height: 2,
-    alt: "IMG_019"
-  },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1js7K8SsO6WTVGwTRjE8GNAZVHesxnHmF",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_003"
-  // },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1CYrD-aF6NivQbaiZQ-Xa8IzjibN6iwtf",
-    width: 2,
-    height: 3,
-    alt: "IMG_022"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1w5zyeHnJo0k69HbvGGvbSSdDRdMzrU3r",
-    width: 2,
-    height: 3,
-    alt: "IMG_023"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=18L2xgKpZz4MPboyGBaOD5pF3sn01M8OM",
-    width: 2,
-    height: 3,
-    alt: "IMG_024"
-  },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1VBuQhRm2ze3wl9tzLkBtxZ3SvVBQMcXb",
-    width: 2,
-    height: 3,
-    alt: "IMG_025"
-  },
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=14KjU1EoG1JhLhvK7qZ55tdGfrGVPWf5X",
-  //   width: 2,
-  //   height: 3,
-  //   alt: "IMG_026"
-  // },
-  {
-    src:
-      "https://drive.google.com/uc?export=view&id=1XkCYJ2Q6ok_NgG8hzpB6ngUrmVN0MYNe",
+    src: "https://drscdn.500px.org/photo/1027984722/q%3D80_m%3D1500/v2?sig=7ed61a9acbe48ad0b74a6ecfab3c7987b1ef7bed71a2f2ee0111e68ede524dd2",
     width: 6,
-    height: 4,
-    alt: "IMG_027"
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984709/q%3D80_m%3D1000/v2?sig=417d469633f04f3c2a5ea2159645e2007638342abfe9d992c359743ca75a09d0",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984773/q%3D80_m%3D1500/v2?sig=2eb5f0633a4b6182e13aa81b5e1cbebfcc3765a5e3803772dafdac3759f296a5",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984752/q%3D80_m%3D1000/v2?sig=10c8301e05be394d897752ff11f07418fe7eaef02c5c5a3c6ebdccae4c7f24b3",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984685/q%3D80_m%3D1000/v2?sig=9518e8167a819af75785f44c57b7d60cb72d078205513136d3843315f574cd6a",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984610/q%3D80_m%3D1500/v2?sig=134e4b7356251de454ababa679ce9df9c6574e1badbd8668aace7adf8cc9fa52",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984579/q%3D80_m%3D1500/v2?sig=74bc168ba162072bcb474549a052fb3ead7d020f2ba52985910b366d61eb22a0",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984836/q%3D80_m%3D1000/v2?sig=5a5ba7a8090e311c5d5667ecc0a53cce2daf495b6af37321e9a13180a86d9347",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984611/q%3D80_m%3D1500/v2?sig=cdf992783de19afbb8849ad5fdc84205d3b1871c296733aad93a43a315caebca",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984687/q%3D80_m%3D1000/v2?sig=0a8202355fde7cbe0d706a81ec8e0b927f06c180db1e0c171301f54cbd04f908",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984835/q%3D80_m%3D1500/v2?sig=abc3ea50c61e8be841d4b964a097022233dda40309cadc24f695679e2746cea1",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984688/q%3D80_m%3D1000/v2?sig=ebb9a25aee0e1669ff8b6406c000558073cb7d4d65c0822fcbd6e0d6244b1983",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984793/q%3D80_m%3D1500/v2?sig=e476f091ff2ecc357dc8de020c7d3e106b2b950356e6390dac0a4ab0617d77a1",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984689/q%3D80_m%3D1000/v2?sig=9e025d1624f4d6dd214c093af5ce1f2fb467909dbbe7a7e992b48dd9d6e97746",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984694/q%3D80_m%3D1000/v2?sig=052c8101b7339f966848429a6fabe3509036ef095e80cac7e9ff8e71d386198e",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984700/q%3D80_m%3D1000/v2?sig=b727e333cc092bef6d5c691f7fced42143fecaa644e14f61441a253538a23be2",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984705/q%3D80_m%3D1000/v2?sig=c3473f32d14395ee4eb67d93759523bc89d32d853884fb0d56fa6b13c87ecad7",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984716/q%3D80_m%3D1000/v2?sig=d817fa423b36ebb550b1752ff692afaa053a90d8399555444f0adb07d37ba248",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984771/q%3D80_m%3D1500/v2?sig=a7f6e5afe130ca4496b9891b8f7e19bf8b6126b46f938c4b60a01050f3aa03d2",
+    width: 6,
+    height: 4
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984725/q%3D80_m%3D1000/v2?sig=16a3740f34c27968ad40c11819ed765aa2174723bc164642945ea7bab01e4930",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984738/q%3D80_m%3D1000/v2?sig=1dc13ede5c4302678253c9288cf492a916a1f67773d2ce8b6de053225301cbad",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984742/q%3D80_m%3D1000/v2?sig=295f8d0c339613be178493e98262612ea170456c6469408442844eb105ade085",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984797/q%3D80_m%3D1000/v2?sig=e56404f59ffe0680e879705386569fdea05fc903c1cec41b7bbe646dfb90b06c",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984822/q%3D80_m%3D1000/v2?sig=0071a722fd493f1f3360f145882197c1e446c951f7d10aa600ca182bb6c7ee27",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027984830/q%3D80_h%3D300/v2?sig=66eb20086e28c977e1f68d3d85e65f658a6d6b7aa92c6c611e84a40e83e26a1d",
+    width: 6,
+    height: 4
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1027984823/q%3D80_m%3D1000/v2?sig=2aef612478a2b176ad457c6cb22159d3fb9afa00a649eaea16ef42cecd685694',
+    width: 2,
+    height: 3
+  },
+  {
+    src: "https://drscdn.500px.org/photo/1027985170/q%3D80_m%3D1000/v2?sig=dee9ee746feaf4ad85a6aaeb2337321eab465321475f4d4880e02bf630971c0c",
+    width: 2,
+    height: 3
   }
-  // {
-  //   src:
-  //     "https://drive.google.com/uc?export=view&id=1a5DZU7xAKK-f4LusQAoHVC7PdYGSihVH",
-  //   width: 6,
-  //   height: 4,
-  //   alt: "IMG_028"
-  // }
 ];
